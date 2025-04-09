a4d141c007a65c5fc3a25acb05df3b8e138c1d6b
ThatNormalTet <cetoprca@gmail.com>
2025-04-09 12:43:04 +0200

    Fixed comments and added compatibility between purged graphs and DFS

--- src/java/com/cesar/Main.java
+++ src/java/com/cesar/Main.java
@@ -6,14 +6,16 @@ import com.cesar.graph.Graph;
 public class Main {
 
     public static void main(String[] args) {
-        Graph graph = new Graph(1, 4, 0, 20, true, false);
+        Graph graph = new Graph(20, 50, 0, 20, true, false);
 
         graph.genNodes();
         graph.genVertices();
+        graph.purge();
 
         graph.exportRelations();
 
-        Search.populate(graph);
+        Search.populateNIDtI(graph);
+        Search.populateNTV(graph);
 
         Search.exploreDFS(graph, graph.nodes.getFirst(), true);
 
--- src/java/com/cesar/search/Search.java
+++ src/java/com/cesar/search/Search.java
@@ -3,14 +3,22 @@ package com.cesar.search;
 import com.cesar.graph.Graph;
 import com.cesar.graph.Node;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Search {
     public static Set<Integer> nodesToVisit = new HashSet<>();
+    private static final Map<Integer, Integer> nodeIDToIndex = new HashMap<>();
+
+    //Maps every nodeID left in the graph to its node list index
+    public static void populateNIDtI(Graph graph){
+        List<Node> nodes = graph.nodes;
+        for (int i = 0; i < nodes.size(); i++) {
+            nodeIDToIndex.put(nodes.get(i).nodeid, i);
+        }
+    }
 
     //Adds to the nodesToVisit list all nodes
-    public static void populate(Graph graph){
+    public static void populateNTV(Graph graph){
         for (Node node : graph.nodes){
             nodesToVisit.add(node.nodeid);
         }
@@ -18,24 +26,27 @@ public class Search {
 
     /**
      * Depth First search algorithm for the graphs generated in this same program.
-     * Not compatible with purged graphs.
      * @param graph Graph the search will take place in.
      * @param node Node to be visited.
      * @param origin Wether this is the node of origin for the current search.
      */
     public static void exploreDFS(Graph graph, Node node, boolean origin){
-        //If the node to be searched has NOT been visited, remove it from the list and add 1 to nodesVisited
+        //If the node to be searched has NOT been visited, remove it from the list
         if (nodesToVisit.contains(node.nodeid)){
             nodesToVisit.remove(node.nodeid);
-            System.out.println(Graph.getID(node.nodeid));
             //search every node the current node points to
             for (int id : node.vertices.keySet()){
-                exploreDFS(graph, graph.nodes.get(id), false);
+                System.out.println(Graph.getID(node.nodeid) + " -> " + Graph.getID(id));
+                try {
+                    exploreDFS(graph, graph.nodes.get(nodeIDToIndex.get(id)), false);
+                }catch (NullPointerException e){
+                    System.out.println("[ WARNING ] DFS reached a dead end corresponding to node " + Graph.getID(id));
+                }
             }
         }
         //If the node is an origin node, but the graph has not been completly visited, visit the first node in nodesToVisit as an origin node
-        if (origin & !nodesToVisit.isEmpty()){
-            exploreDFS(graph, graph.nodes.get((Integer) nodesToVisit.toArray()[0]), true);
+        if (origin & !nodesToVisit.isEmpty()) {
+            exploreDFS(graph, graph.nodes.get(nodeIDToIndex.get(nodesToVisit.iterator().next())), true);
         }
     }
 }
